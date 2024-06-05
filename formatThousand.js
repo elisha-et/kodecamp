d70853d65dee999b375b4d64dbbf834ac8fabf74
hacker-elisha <etukudohelisha@gmail.com>
2024-06-05 19:18:04 +0100

    Corrected error in formatThousand function

--- formatThousand.js
+++ formatThousand.js
@@ -1,21 +1,9 @@
-function formatThousand(number){
-    if(typeof number === "number"){
-        let numStr = number.toString()
-        if(numStr.length > 3){
-            numStr = numStr.split("")
-        for(let i = numStr.length - 3; i >= 0; i-=3){
-            numStr.splice(i,0,",")
-        }
-        numStr = numStr.join("")
-        return(numStr)
-        }
-        else{
-            return(numStr)
-        }
-    }
-    else{
-        return("The function only works with numbers.")
+function formatThousand(number) {
+    if (typeof number === "number") {
+        return number.toLocaleString();
+    } else {
+        return "The function only works with numbers.";
     }
 }
 
-console.log(formatThousand(1000))
+console.log(formatThousand(1000));
